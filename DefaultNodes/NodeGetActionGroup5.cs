3a79891d92c01c35b80c901451bd66da09bc8fce
aftokinito <carmaster@game-hunters.com>
2014-10-31 09:57:39 +0100

    Added ActionGroup nodes

---
+++ DefaultNodes/NodeGetActionGroup5.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KSPComputer;
+using KSPComputer.Nodes;
+using KSPComputer.Connectors;
+namespace DefaultNodes
+{
+    [Serializable]
+    public class NodeGetActionGroup5 : RootNode
+    {
+        protected override void OnCreate()
+        {
+            Out<bool>("State");
+            Program.OnTick += Program_OnTick;
+        }
+        protected override void OnExecute(ConnectorIn input)
+        {
+            Out("State", Program.Vessel.ActionGroups[KSPActionGroup.Custom05]);
+            ExecuteNext();
+        }
+
+        void Program_OnTick()
+        {
+            Execute(null);
+        }
+
+        protected override void OnDestroy()
+        {
+            Program.OnTick -= Program_OnTick;
+        }
+    }
+}
---
+++ DefaultNodes/NodeSetActionGroup8.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KSPComputer.Nodes;
+using KSPComputer.Connectors;
+namespace DefaultNodes
+{
+    [Serializable]
+    public class NodeSetActionGroup8 : ExecutableNode
+    {
+        protected override void OnCreate()
+        {
+            In<bool>("State");
+        }
+        protected override void OnExecute(ConnectorIn input)
+        {
+            var state = In("State").AsBool();
+            Program.Vessel.ActionGroups.SetGroup(KSPActionGroup.Custom08, state);
+            ExecuteNext();
+        }
+    }
+}
