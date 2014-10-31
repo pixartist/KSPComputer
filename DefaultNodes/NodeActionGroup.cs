0172b261db50c32193e782f751eaf517dca1bfb0
aftokinito <carmaster@game-hunters.com>
2014-10-31 15:23:08 +0100

    Made the array a const

--- DefaultNodes/NodeActionGroup.cs
+++ DefaultNodes/NodeActionGroup.cs
@@ -10,33 +10,32 @@ namespace DefaultNodes
     [Serializable]
     public class NodeActionGroup : ExecutableNode
     {
-        protected KSPActionGroup[] AGroups;
         protected int inputID;
+        protected const KSPActionGroup[] AGroups = new KSPActionGroup[]
+        {
+            KSPActionGroup.Custom01,
+            KSPActionGroup.Custom02,
+            KSPActionGroup.Custom03,
+            KSPActionGroup.Custom04,
+            KSPActionGroup.Custom05,
+            KSPActionGroup.Custom06,
+            KSPActionGroup.Custom07,
+            KSPActionGroup.Custom08,
+            KSPActionGroup.Custom09,
+            KSPActionGroup.Custom10,
+            KSPActionGroup.Abort,
+            KSPActionGroup.Brakes,
+            KSPActionGroup.Gear,
+            KSPActionGroup.Light,
+            KSPActionGroup.RCS,
+            KSPActionGroup.SAS,
+            KSPActionGroup.Stage
+        };
         protected override void OnCreate()
         {
             In<double>("ActionID");
             In<bool>("Set");
             Out<bool>("Enabled");
-
-            AGroups = new KSPActionGroup[17];
-            AGroups[0] = KSPActionGroup.Custom01;
-            AGroups[1] = KSPActionGroup.Custom02;
-            AGroups[2] = KSPActionGroup.Custom03;
-            AGroups[3] = KSPActionGroup.Custom04;
-            AGroups[4] = KSPActionGroup.Custom05;
-            AGroups[5] = KSPActionGroup.Custom06;
-            AGroups[6] = KSPActionGroup.Custom07;
-            AGroups[7] = KSPActionGroup.Custom08;
-            AGroups[8] = KSPActionGroup.Custom09;
-            AGroups[9] = KSPActionGroup.Custom10;
-            AGroups[10] = KSPActionGroup.Abort;
-            AGroups[11] = KSPActionGroup.Brakes;
-            AGroups[12] = KSPActionGroup.Gear;
-            AGroups[13] = KSPActionGroup.Light;
-            AGroups[14] = KSPActionGroup.RCS;
-            AGroups[15] = KSPActionGroup.SAS;
-            AGroups[16] = KSPActionGroup.Stage;
-        }
         protected override void OnExecute(ConnectorIn input)
         {
 
